7d03496bd9696648243935e4648a75e766ca3a62
Anthony <awessel@mail.bradley.edu>
2020-10-06 15:22:37 -0500

    Finished 2D Level

---
+++ PennyPixel_2DTilemapProject/Assets/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+    public static bool gameover;
+
+    public Text scoreText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameover)
+        {
+            scoreText.text = "You Win!\nYou collected " + score + " gems!";
+        }
+        else
+        {
+            scoreText.text = "Score: " + score;
+        }
+    }
+}
