70db16f76a3d32ac0db2d4d15cf056c1ad1457a1
Anthony <awessel@mail.bradley.edu>
2020-10-07 17:34:32 -0500

    Started work on new level

---
+++ 3Dprototype/Assets/MyFirstPersonPlayer/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    public GameObject player;
+    private float verticalLookRotation = 0f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        player.transform.Rotate(Vector3.up * mouseX);
+
+        // Calculate camera rotation
+        verticalLookRotation -= mouseY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+        // Apply camera rotation
+        transform.localRotation = Quaternion.Euler(verticalLookRotation, 0, 0);
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ 3Dprototype/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycast.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRaycast : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+
+    public ParticleSystem muzzleFlash;
+
+    public float hitForce = 10f;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        {
+            Target target = hitInfo.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
