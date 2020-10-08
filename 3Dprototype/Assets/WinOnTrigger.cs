ba33fae678bd71c890d5beaa2a3fff079f47374b
Anthony <awessel@mail.bradley.edu>
2020-10-07 23:59:44 -0500

    Finished new level

---
+++ 3Dprototype/Assets/WinOnTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinOnTrigger : MonoBehaviour
+{
+    public Text winText;
+
+    bool gameover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameover = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameover)
+        {
+            winText.text = "You win!\nPress R to play again!";
+            if (Input.GetKeyDown(KeyCode.R))
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameover = true;
+        }
+    }
+}
